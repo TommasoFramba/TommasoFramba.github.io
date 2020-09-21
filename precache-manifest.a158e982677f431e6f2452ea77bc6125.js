d786d60a6e8f3988c12dad253a030472874b1c42
Tommaso Marco Framba <frambatom@gmail.com>
2020-09-20 18:40:01 -0700

    Updates

---
+++ precache-manifest.a158e982677f431e6f2452ea77bc6125.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90d5410f1f8b4590f83c0fcd36290549",
+    "url": "/react-deploy/index.html"
+  },
+  {
+    "revision": "b8548cf1c4c9c9583b03",
+    "url": "/react-deploy/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "231e9ef4d1682c19b164",
+    "url": "/react-deploy/static/css/main.8f6a8295.chunk.css"
+  },
+  {
+    "revision": "b8548cf1c4c9c9583b03",
+    "url": "/react-deploy/static/js/2.d126c128.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-deploy/static/js/2.d126c128.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "231e9ef4d1682c19b164",
+    "url": "/react-deploy/static/js/main.ce9f0a04.chunk.js"
+  },
+  {
+    "revision": "3e6ae98d5a9411ec2365",
+    "url": "/react-deploy/static/js/runtime-main.2e59d031.js"
+  },
+  {
+    "revision": "f391e657f9cabe91f018ca14e340d693",
+    "url": "/react-deploy/static/media/logo.f391e657.svg"
+  }
+]);
