48aa0546dd535b7b38eae2b56bb7136ce8293a11
Tommaso Marco Framba <frambatom@gmail.com>
2020-09-20 19:11:43 -0700

    Updates

---
+++ precache-manifest.51fd1cee1007d08b890057614bb52f18.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "399490eb262311260edb5990a17915af",
+    "url": "/TommasoFr.github.io/index.html"
+  },
+  {
+    "revision": "66477d34395a170764af",
+    "url": "/TommasoFr.github.io/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "bca66f6eaac1a8c4fcee",
+    "url": "/TommasoFr.github.io/static/css/main.42e527ef.chunk.css"
+  },
+  {
+    "revision": "66477d34395a170764af",
+    "url": "/TommasoFr.github.io/static/js/2.aa596bf8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/TommasoFr.github.io/static/js/2.aa596bf8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bca66f6eaac1a8c4fcee",
+    "url": "/TommasoFr.github.io/static/js/main.34bc0974.chunk.js"
+  },
+  {
+    "revision": "7ab5aec11d78114c4de6",
+    "url": "/TommasoFr.github.io/static/js/runtime-main.e78bc01c.js"
+  },
+  {
+    "revision": "f391e657f9cabe91f018ca14e340d693",
+    "url": "/TommasoFr.github.io/static/media/logo.f391e657.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.36b0a47704e41bc40a96f7bb78246073.js"
+  "/TommasoFr.github.io/precache-manifest.51fd1cee1007d08b890057614bb52f18.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/TommasoFr.github.io/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
