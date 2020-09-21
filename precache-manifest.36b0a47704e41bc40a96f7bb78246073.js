110158d9fe0db971377f090e5644635d0d10153b
Tommaso Marco Framba <frambatom@gmail.com>
2020-09-20 19:10:13 -0700

    Updates

---
+++ precache-manifest.36b0a47704e41bc40a96f7bb78246073.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87594563c9419f96ed414718aa39bddf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f33ee73cad257a47b0cd",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "492254b1c0cf87e3a310",
+    "url": "/static/css/main.42e527ef.chunk.css"
+  },
+  {
+    "revision": "f33ee73cad257a47b0cd",
+    "url": "/static/js/2.127d5836.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.127d5836.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "492254b1c0cf87e3a310",
+    "url": "/static/js/main.f7a8d4df.chunk.js"
+  },
+  {
+    "revision": "e772e3ee0db6f9829b44",
+    "url": "/static/js/runtime-main.fdaf22cd.js"
+  },
+  {
+    "revision": "f391e657f9cabe91f018ca14e340d693",
+    "url": "/static/media/logo.f391e657.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-deploy/precache-manifest.a70fe814a40d7dc3307022932fd35b10.js"
+  "/precache-manifest.36b0a47704e41bc40a96f7bb78246073.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-deploy/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
